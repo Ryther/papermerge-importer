68ec7a5490b15d90a7feb7ef86c1023fd279f372
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2021-02-04 16:00:58 +0100

    Execute curl only on filename not empty

--- papermerge-importer.sh
+++ papermerge-importer.sh
@@ -1,23 +1,25 @@
 #!/usr/bin/with-contenv bash
 # shellcheck shell=bash
 
-if [ -z ${PAPERMERGE_HOST+x} ]; then
+if [ -z "${PAPERMERGE_HOST+x}" ]; then
     echo "[papermerge-importer.sh] Cannot find PAPERMERGE_HOST env variable"
     exit
 fi
 
-if [ -z ${AUTH_TOKEN+x} ]; then
+if [ -z "${AUTH_TOKEN+x}" ]; then
     echo "[papermerge-importer.sh] Cannot find AUTH_TOKEN env variable"
     exit
 fi
 
-inotifywait -m "/data/papermerge/import" -e move,create,close  |
+inotifywait -r -m "/data/papermerge/import" -e move,create,close  |
 while read -r directory events filename; do
-    echo "[papermerge-importer.sh] Found new file: ${filename}"
-    curl -H "Authorization: Token ${AUTH_TOKEN}"  \
-        -T "${directory}/${filename}"  \
-        "${PAPERMERGE_HOST}/api/document/upload/${filename}" || \
-            echo "[papermerge-importer.sh] curl: error ${?}"
-    rm -f "${directory}/${filename}" || \
-            echo "[papermerge-importer.sh] rm file: error ${?}"
+    if [ -n "${filename+x}" ]; then
+        echo "[papermerge-importer.sh] Found new file: ${filename}"
+        curl -H "Authorization: Token ${AUTH_TOKEN}"  \
+            -T "${directory}/${filename}"  \
+            "${PAPERMERGE_HOST}/api/document/upload/${filename}" || \
+                echo "[papermerge-importer.sh] curl: error ${?}"
+        rm -f "${directory}/${filename}" || \
+                echo "[papermerge-importer.sh] rm file: error ${?}"
+    fi
 done
