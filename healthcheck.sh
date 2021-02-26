09b615558caefe00826efc5f779ce429475d147a
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2021-02-26 19:50:32 +0100

    Fix to healthcheck [5]

--- healthcheck.sh
+++ healthcheck.sh
@@ -2,12 +2,15 @@
 # shellcheck shell=bash
 
 source /tmp/healthcheck.status
+echo "returnCode=${returnCode}"
 
 case $returnCode in
     0)
+        echo "returnCode=${returnCode}"
         exit 0
     ;;
     *)
+        echo "returnCode=${returnCode}"
         exit 1
     ;;
 esac
