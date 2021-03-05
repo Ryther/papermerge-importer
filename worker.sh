19d021e27cbcdb0a0dab402f9ba6dbc80f65f366
Edoardo Zanoni <edoardo.zanoni@gmail.com>
2021-03-05 12:36:18 +0100

    Improved script readability [4]

--- worker.sh
+++ worker.sh
@@ -6,13 +6,13 @@ upload() {
     local worker_file="$2"
     if [ -n "${worker_file+x}" ] && [ ! "${worker_file}" = "" ]; then
         log "$0" 1 "Found new file | Filename: ${worker_file}"
-        result_code=$(sendFile "${worker_dir}" "${worker_file}")
+        result_code="$(sendFile "${worker_dir}" "${worker_file}")"
         case $result_code in
             200)
-                resultOK worker_dir worker_file
+                resultOK "${worker_dir}" "${worker_file}"
             ;;
             *)
-                resultKO result_code
+                resultKO "${result_code}"
             ;;
         esac
     fi
@@ -22,10 +22,11 @@ sendFile() {
     local send_dir="$1"
     local send_file="$2"
     log "$0" 1 "curl -s -o /tmp/api_response -w \"%{http_code}\" -H \"Authorization: Token ${AUTH_TOKEN:0:3}...${AUTH_TOKEN: -3}\" -T \"${worker_directory}${filename}\" \"${PAPERMERGE_HOST}/api/document/upload/${filename}\""
-    curl -s -o /tmp/api_response -w "%{http_code}" \
+    # shellcheck disable=SC2005
+    echo "$(curl -s -o /tmp/api_response -w "%{http_code}" \
                     -H "Authorization: Token ${AUTH_TOKEN}" \
                     -T "${send_dir}${send_file}" \
-                    "${PAPERMERGE_HOST}/api/document/upload/${send_file}"
+                    "${PAPERMERGE_HOST}/api/document/upload/${send_file}")"
 }
 
 resultOK() {
